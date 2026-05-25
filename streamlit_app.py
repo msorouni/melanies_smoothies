716e46f826b96338b88ce6cf95cba066034a565c
Michael <37784193+msorouni@users.noreply.github.com>
2026-05-25 23:09:26 +1000

    Update dataframe handling to use Pandas
    Convert Snowflake DataFrame to Pandas DataFrame for display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import os
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests  
+import pandas as pd
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customise Your Smoothie! :cup_with_straw:")
@@ -26,8 +27,11 @@ st.write("The name on the Smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+#st.dataframe(data=my_dataframe, use_container_width=True)
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+#st.stop()
+
                                                                       
 ingredients_list = st.multiselect(
     'Choose up to 5 ingedients:',
