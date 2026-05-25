ec613cef89161c8594b7dcee4cf4eead02c10885
Michael <37784193+msorouni@users.noreply.github.com>
2026-05-25 22:44:55 +1000

    Fetch smoothie data from API and display in app

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import os
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests  
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customise Your Smoothie! :cup_with_straw:")
@@ -41,6 +42,10 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:  
         ingredients_string += fruit_chosen + ' '
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+      #st.text(smoothiefroot_response.json())
+      sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
         
     st.write(ingredients_string)    
 
@@ -56,7 +61,4 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered, ' + name_on_order + '!')
 
-import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
