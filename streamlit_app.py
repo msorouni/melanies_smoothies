528f17c58c06c0e64aff297dac78226ce693ed6a
Michael <37784193+msorouni@users.noreply.github.com>
2026-05-25 22:37:54 +1000

    Fix URL formatting in API request for watermelon

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,5 +57,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, ' + name_on_order + '!')
 
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response)
